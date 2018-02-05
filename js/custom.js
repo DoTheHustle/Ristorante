545de1e0c1e213c594de67782278c9c6e12f18d4
IhToN <ihton93@gmail.com>
2018-02-05 14:10:51 +0100

    MDB Styling - blankpage NOT IN USE

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+// SideNav Initialization
+//$(".button-collapse").sideNav();
+
+//Animation init
+new WOW().init();
+
+//Modal
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+})
+
+// Material Select Initialization
+$(document).ready(function () {
+    $('.mdb-select').material_select();
+});
+
+// MDB Lightbox Init
+$(function () {
+    $("#mdb-lightbox-ui").load("../mdb-addons/mdb-lightbox-ui.html");
+});
